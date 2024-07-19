4a40700c7d3a69d15f3b2ad6d3deb3437eb0708f
Albert Pangestu <stamina.left@gmail.com>
2024-07-20 00:23:18 +0700

    Created test case, when the `repository` request to the `client`, it should return a `response`

--- LoremIpsumGeneratorTests/Repository/RemoteGenerateLoremIpsumRepositoryTest.swift
+++ LoremIpsumGeneratorTests/Repository/RemoteGenerateLoremIpsumRepositoryTest.swift
@@ -15,14 +15,13 @@ class RemoteGenerateLoremIpsumRepository: GenerateLoremIpsumRepository {
         self.client = client
     }
     
-    func generateLoremIpsum(numberOfParagraphs: Int) async throws -> LoremIpsumGenerator.TextResponse {
-        client.get()
-        return TextResponse(text: "")
+    func generateLoremIpsum(numberOfParagraphs: Int) async throws -> TextResponse {
+        try await client.get(numberOfParagraphs: numberOfParagraphs)
     }
 }
 
 protocol HTTPClient {
-    func get()
+    func get(numberOfParagraphs: Int) async throws -> TextResponse
 }
 
 final class RemoteGenerateLoremIpsumRepositoryTest: XCTestCase {
@@ -50,6 +49,21 @@ final class RemoteGenerateLoremIpsumRepositoryTest: XCTestCase {
         XCTAssertEqual(client.requestCount, 2)
     }
     
+    func test_whenGenerate_shouldReturnResponse() async throws {
+        let text = TextResponse(text: "Lorem ipsum tincidunt vitae semper quis lectus.\n")
+        let client = HTTPClientStub(result: .success(text))
+        let sut = RemoteGenerateLoremIpsumRepository(client: client)
+        var capturedText: TextResponse?
+        
+        do {
+            capturedText = try await sut.generateLoremIpsum(numberOfParagraphs: 1)
+        } catch {
+            XCTFail("Should be success, error is not an expectation")
+        }
+        
+        XCTAssertEqual(capturedText, text)
+    }
+    
     // MARK: - Helpers
     private func makeSUT() -> (sut: RemoteGenerateLoremIpsumRepository, HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -61,8 +75,21 @@ final class RemoteGenerateLoremIpsumRepositoryTest: XCTestCase {
     final class HTTPClientSpy: HTTPClient {
         private(set) var requestCount: Int = 0
         
-        func get() {
+        func get(numberOfParagraphs: Int) async throws -> TextResponse {
             requestCount += 1
+            return TextResponse(text: "")
+        }
+    }
+    
+    final class HTTPClientStub: HTTPClient {
+        private(set) var result: Result<TextResponse, Error>
+        
+        init(result: Result<TextResponse, Error>) {
+            self.result = result
+        }
+        
+        func get(numberOfParagraphs: Int) async throws -> TextResponse {
+            try result.get()
         }
     }
 }
