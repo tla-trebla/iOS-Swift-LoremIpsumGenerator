248a480e0b672c60244275be65b2aa3914d7225e
Albert Pangestu <stamina.left@gmail.com>
2024-07-21 01:43:22 +0700

    Removed unused files and set the into empty view on the `LoremIpsumApp` file

---
+++ App/LoremIpsum/LoremIpsum/LoremIpsumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoremIpsumApp.swift
+//  LoremIpsum
+//
+//  Created by Albert Pangestu on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LoremIpsumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
