b1278902178ecf7e0098bf098a8364ef6e5dbc6c
Albert Pangestu <stamina.left@gmail.com>
2024-07-26 00:30:47 +0700

    Improved test case of `ContentView`'s initialization, by adding a `viewModel`, as the `StateObject`

--- App/LoremIpsum/LoremIpsumTests/ContentView/LoremIpsumGeneratorContentViewTest.swift
+++ App/LoremIpsum/LoremIpsumTests/ContentView/LoremIpsumGeneratorContentViewTest.swift
@@ -5,10 +5,14 @@
 //  Created by Albert Pangestu on 24/07/24.
 //
 
+@testable import LoremIpsum
 import XCTest
+import LoremIpsumLib
 import SwiftUI
 
 struct LoremIpsumGeneratorContentView: View {
+    @StateObject var viewModel: LoremIpsumGeneratorViewModel
+    
     var body: some View {
         EmptyView()
     }
@@ -17,9 +21,25 @@ struct LoremIpsumGeneratorContentView: View {
 final class LoremIpsumGeneratorContentViewTest: XCTestCase {
 
     func test_initialize_canBeInitialized() {
-        let sut = LoremIpsumGeneratorContentView()
+        let useCase = GenerateLoremIpsumUseCaseDummy()
+        let service = ClipboardServiceDummy()
+        let viewModel = LoremIpsumGeneratorViewModel(useCase: useCase, clipboardService: service)
+        let sut = LoremIpsumGeneratorContentView(viewModel: viewModel)
         
         XCTAssertNotNil(sut)
+    }
+    
+    // MARK: - Helpers
+    final class GenerateLoremIpsumUseCaseDummy: GenerateLoremIpsumUseCase {
+        func generateLoremIpsum(numberOfParagraphs: Int) async throws -> LoremIpsumLib.TextResponse {
+            let data = """
+""".data(using: .utf8)!
+            return try JSONDecoder().decode(TextResponse.self, from: data)
+        }
+    }
+    
+    final class ClipboardServiceDummy: ClipboardService {
+        func copy(_ text: String) { }
     }
 
 }
