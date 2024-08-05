f6b34864c435e58a2de263a51a53bc536a963ed6
Albert Pangestu <stamina.left@gmail.com>
2024-08-05 23:22:31 +0700

    Created `ScrollableTextView` to make the `Text` scrollable and able to be copy and paste due to SwiftUI's limitation, adjusted the custom view 60% of the phone screen, moved the input and button into bottom side, and set the input and button automatically moved to the top each time user try to input via keyboard

--- App/LoremIpsum/LoremIpsum/LoremIpsumGeneratorContentView.swift
+++ App/LoremIpsum/LoremIpsum/LoremIpsumGeneratorContentView.swift
@@ -10,41 +10,118 @@ import Domain
 
 struct LoremIpsumGeneratorContentView: View {
     @StateObject var viewModel: LoremIpsumGeneratorViewModel
+    @State private var keyboardHeight: CGFloat = 0
     @State private var numberOfParagraphs: String = ""
     
     var body: some View {
-        ScrollView {
+        GeometryReader { geometry in
             VStack {
-                TextField("Number of Paragraphs", text: $numberOfParagraphs)
-                    .keyboardType(.numberPad)
-                    .onChange(of: numberOfParagraphs) { _, newValue in
-                        numberOfParagraphs = newValue.filter({ "0123456789".contains($0) })
-                    }
-                    .multilineTextAlignment(.center)
-                    .padding()
+                ScrollableTextView(text: viewModel.generatedText,
+                                   onLongPress: {
+                    viewModel.copyGeneratedText()
+                })
+                .frame(maxHeight: geometry.size.height * 0.6)
                 
-                Button(action: {
-                    Task {
-                        try await viewModel.generateLoremIpsum(numberOfParagraphs: Int(numberOfParagraphs)!)
-                    }
-                }, label: {
-                    Text("Generate")
-                }).padding()
+                Spacer()
                 
-                if let errorMessage = viewModel.errorMessage {
-                    Text(errorMessage)
-                        .foregroundStyle(.red)
+                VStack {
+                    if let errorMessage = viewModel.errorMessage {
+                        Text(errorMessage)
+                            .foregroundStyle(.red)
+                            .padding()
+                    }
+                    
+                    TextField("Number of Paragraphs", text: $numberOfParagraphs)
+                        .keyboardType(.numberPad)
+                        .onChange(of: numberOfParagraphs) { _, newValue in
+                            numberOfParagraphs = newValue.filter({ "0123456789".contains($0) })
+                        }
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Button(action: {
+                        Task {
+                            try await viewModel.generateLoremIpsum(numberOfParagraphs: Int(numberOfParagraphs)!)
+                        }
+                    }, label: {
+                        Text("Generate")
+                    }).padding()
                 }
-                
-                Text(viewModel.generatedText)
-                    .onLongPressGesture {
-                        viewModel.copyGeneratedText()
-                    }.padding()
+                .padding()
+                .padding(.bottom, keyboardHeight)
+                .animation(.easeOut, value: 0.3)
             }
+            .onAppear(perform: subscribeToKeyboardEvents)
+            .ignoresSafeArea(edges: .bottom)
+        }
+    }
+    
+    private func subscribeToKeyboardEvents() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+            if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                keyboardHeight = keyboardFrame.height
+            }
+        }
+        
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
+            keyboardHeight = 0
         }
     }
 }
 
 #Preview {
     LoremIpsumGeneratorContentView(viewModel: LoremIpsumGeneratorViewModel(useCase: RemoteGenerateLoremIpsumUseCase(), clipboardService: UIKitClipboardService()))
+}
+
+struct ScrollableTextView: UIViewRepresentable {
+    var text: String
+    var onLongPress: () -> Void
+    
+    class Coordinator: NSObject, UIGestureRecognizerDelegate {
+        var parent: ScrollableTextView
+        
+        init(parent: ScrollableTextView) {
+            self.parent = parent
+        }
+        
+        @objc func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer) {
+            if gesture.state == .began {
+                parent.onLongPress()
+            }
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    func makeUIView(context: Context) -> some UIView {
+        let scrollView = UIScrollView()
+        let textView = UILabel()
+        
+        textView.text = text
+        textView.numberOfLines = 0
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(textView)
+        NSLayoutConstraint.activate([
+            textView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            textView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            textView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            textView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleLongPressGesture(_:)))
+        longPressGesture.delegate = context.coordinator
+        scrollView.addGestureRecognizer(longPressGesture)
+        
+        return scrollView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        if let textView = uiView.subviews.first as? UILabel {
+            textView.text = text
+        }
+    }
 }
